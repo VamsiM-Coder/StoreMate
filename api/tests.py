2ab16494867089dd7b4bd3f99d922d73fe4dcf01
Vamsi Mandala <mandalavamsi372@gmail.com>
2025-08-08 22:19:09 +0530

    added commit

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,80 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class DiffValueAPITestCase(APITestCase):
+    def get_dict_diff_based_on_original_value(self, current_value, original_value, key,
+            value, diff_dict, original_dict):
+        if(isinstance(original_value, list)):
+            return value, original_value
+        if (value != original_value.get(key)):
+                    diff_dict[key] = value
+                    original_dict[key] = original_value.get(key)
+                    return diff_dict, original_dict
+    
+    def test_list_to_list(self):
+        original_value = ['Pending']
+        current_value = ['Success']
+        key = "Emi_status"
+        value = 'Success'
+        diff_dict = {}
+        original_dict = {}
+
+        expected = ('Success', ['Pending'])
+
+        result = self.get_dict_diff_based_on_original_value(
+            current_value, original_value, key, value, diff_dict, original_dict
+        )
+
+        self.assertEqual(result, expected)
+        
+    def test_dict_to_dict(self):
+        original_value = {"Emi_status": "Pending"}
+        current_value = {"Emi_status": "Success"}
+        key = "Emi_status"
+        value = 'Success'
+        diff_dict = {}
+        original_dict = {}
+
+        expected = {"Emi_status": "Success"}, {"Emi_status": "Pending"}
+
+        result = self.get_dict_diff_based_on_original_value(
+            current_value, original_value, key, value, diff_dict, original_dict
+        )
+
+        self.assertEqual(result, expected)    
+        
+        
+    def test_list_to_dict(self):
+        original_value = ['Pending']
+        current_value = {"Emi_status": "Success"}
+        key = "Emi_status"
+        value = 'Success'
+        diff_dict = {}
+        original_dict = {}
+
+        expected = ('Success', ['Pending'])
+
+        result = self.get_dict_diff_based_on_original_value(
+            current_value, original_value, key, value, diff_dict, original_dict
+        )
+
+        self.assertEqual(result, expected)
+        
+        
+    def test_dict_to_list(self):
+        original_value = {"Emi_status": "Pending"}
+        current_value =  ["Success"]
+        key = "Emi_status"
+        value = 'Success'
+        diff_dict = {}
+        original_dict = {}
+
+        expected = {"Emi_status": "Success"}, {"Emi_status": "Pending"}
+
+        result = self.get_dict_diff_based_on_original_value(
+            current_value, original_value, key, value, diff_dict, original_dict
+        )
+
+        self.assertEqual(result, expected)
+            
+ 
+            
